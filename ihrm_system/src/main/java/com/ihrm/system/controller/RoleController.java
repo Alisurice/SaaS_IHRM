b64ff0c0806cb99aa9c1afed10591209e79d76c2
test <test>
2021-12-23 14:55:22 +0800

    用户和角色管理

---
+++ ihrm_system/src/main/java/com/ihrm/system/controller/RoleController.java
@@ -0,0 +1,82 @@
+package com.ihrm.system.controller;
+
+import com.ihrm.common.controller.BaseController;
+import com.ihrm.common.entity.PageResult;
+import com.ihrm.common.entity.Result;
+import com.ihrm.common.entity.ResultCode;
+import com.ihrm.domain.system.Role;
+import com.ihrm.domain.system.response.RoleResult;
+import com.ihrm.system.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/sys")
+public class RoleController extends BaseController {
+    @Autowired
+    private RoleService roleService;
+
+    //添加角色
+    @RequestMapping(value = "/role",method = RequestMethod.POST)
+    public Result add(@RequestBody Role role){
+        role.setCompanyId(super.companyId);
+        roleService.save(role);
+        return new Result(ResultCode.SUCCESS);
+    }
+
+
+
+    /**
+     * 修改角色
+     */
+    @RequestMapping(value = "/role/{id}" , method = RequestMethod.PUT)
+    public Result update(@PathVariable(value = "id") String id , @RequestBody Role role){
+        //设置修改的用户Id
+        role.setId(id);
+        //调用Service更新
+        roleService.update(role);
+        return new Result(ResultCode.SUCCESS);
+    }
+
+    /**
+     * 根据ID获取角色信息
+     */
+    @RequestMapping(value = "/role/{id}", method = RequestMethod.GET)
+    public Result findById(@PathVariable(name = "id") String id) throws Exception {
+        Role role = roleService.findById(id);
+        RoleResult roleResult = new RoleResult(role);
+        return new Result(ResultCode.SUCCESS,roleResult);
+    }
+
+    /**
+     * 根据Id删除
+     */
+    @RequestMapping(value = "/role/{id}" , method = RequestMethod.DELETE)
+    public Result delete(@PathVariable(value = "id") String id){
+        roleService.delete(id);
+        return new Result(ResultCode.SUCCESS);
+    }
+
+    /**
+     * 分页查询角色
+     */
+    @RequestMapping(value = "/role", method = RequestMethod.GET)
+    public Result findByPage(int page,int pagesize) throws Exception {
+        Page<Role> searchPage = roleService.findByPage(companyId, page, pagesize);
+        PageResult<Role> pr = new PageResult(searchPage.getTotalElements(),searchPage.getContent());
+        return new Result(ResultCode.SUCCESS,pr);
+    }
+
+    @RequestMapping(value = "/role/list" , method = RequestMethod.GET)
+    public Result findAll(){
+        List<Role> roleList = roleService.findAll(super.companyId);
+        return new Result(ResultCode.SUCCESS , roleList);
+    }
+
+
+}
---
+++ ihrm_system/src/main/java/com/ihrm/system/controller/UserController.java
@@ -0,0 +1,110 @@
+package com.ihrm.system.controller;
+
+import com.ihrm.common.controller.BaseController;
+import com.ihrm.common.entity.PageResult;
+import com.ihrm.common.entity.Result;
+import com.ihrm.common.entity.ResultCode;
+import com.ihrm.domain.system.User;
+import com.ihrm.domain.system.response.UserResult;
+import com.ihrm.system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+
+//解决跨域
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/sys")
+public class UserController extends BaseController {
+
+    @Autowired
+    private UserService userService;
+
+
+    /**
+     * 分配角色
+     */
+    @RequestMapping(value = "/user/assignRoles" , method = RequestMethod.PUT)
+    public Result save(@RequestBody Map<String,Object> map){
+
+        //获取被分配的用户id
+        String userId = (String) map.get("id");
+        //获取到角色的id列表
+        List<String> roleIds = (List<String>) map.get("roleIds");
+        //调用service完成角色分配
+        userService.assignRoles(userId , roleIds);
+
+        return new Result(ResultCode.SUCCESS);
+    }
+
+    /**
+     * 保存
+     * @return
+     */
+    @RequestMapping(value = "/user" , method = RequestMethod.POST)
+    public Result save(@RequestBody User user)  {
+        //设置保存的用户id
+        user.setCompanyId(super.companyId);
+        user.setCompanyName(companyName);
+        //调用service完成保存用户
+        userService.save(user);
+        //构造返回结果
+        return new Result(ResultCode.SUCCESS);
+    }
+
+    /**
+     * 查询用户列表
+     * @return
+     */
+    @RequestMapping(value = "/user" , method = RequestMethod.GET)
+    public Result findAll(@RequestParam(value = "page",defaultValue = "1") int page,
+                          @RequestParam(value = "size",defaultValue = "10")int size,
+                          @RequestParam Map map){
+
+        //获取当前的企业id
+        map.put("companyId" , super.companyId);
+
+        Page<User> pageUser = userService.findAll(map, page, size);
+        //构造返回结果
+        PageResult<User> pageResult = new PageResult<>(pageUser.getTotalElements(),pageUser.getContent());
+        return new Result(ResultCode.SUCCESS , pageResult);
+    }
+
+
+    /**
+     * 根据Id查询
+     */
+    @RequestMapping(value = "/user/{id}" , method = RequestMethod.GET)
+    public Result findById(@PathVariable(value = "id") String id){
+        //添加roleIds(用户已经具有的角色id数组)
+        User user = userService.findById(id);
+        UserResult userResult = new UserResult(user);
+        return new Result(ResultCode.SUCCESS , userResult);
+    }
+
+    /**
+     * 修改User
+     */
+    @RequestMapping(value = "/user/{id}" , method = RequestMethod.PUT)
+    public Result update(@PathVariable(value = "id") String id , @RequestBody User user){
+        //设置修改的用户Id
+        user.setId(id);
+        //调用Service更新
+        userService.update(user);
+        return new Result(ResultCode.SUCCESS);
+    }
+
+    /**
+     * 根据Id删除
+     */
+    @RequestMapping(value = "/user/{id}" , method = RequestMethod.DELETE)
+    public Result delete(@PathVariable(value = "id") String id){
+        userService.deleteById(id);
+        return new Result(ResultCode.SUCCESS);
+    }
+
+}
