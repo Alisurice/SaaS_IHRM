2f2702409be3b550a5c40777eab9424d61c747be
test <test>
2021-12-30 22:43:54 +0800

    feat 百万数据excel的导入和导出 todo 目前前端无法下载

--- ihrm_employee/src/main/java/com/ihrm/employee/controller/EmployeeController.java
+++ ihrm_employee/src/main/java/com/ihrm/employee/controller/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.ihrm.employee.controller;
 
-
 import com.ihrm.common.controller.BaseController;
 import com.ihrm.common.entity.PageResult;
 import com.ihrm.common.entity.Result;
@@ -21,12 +20,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 @RestController
 @CrossOrigin
@@ -214,123 +212,24 @@ public class EmployeeController extends BaseController {
         return new Result(ResultCode.SUCCESS,pr);
     }
 
-    /**
-     * todo 目前前端无法下载
-     * @throws Exception
-     */
-    //@RequestMapping(value = "/template", method = RequestMethod.GET)
-    public void getTemplate() throws Exception {
-        Resource resource = new ClassPathResource("excel-template/employee-template.xlsx");
-        try(FileInputStream fin = new FileInputStream(resource.getFile())){
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            byte[] b = new byte[1024];
-            int i = 0;
-            while ((i = fin.read(b)) != -1) {
 
-                out.write(b, 0, b.length);
-            }
-            new DownloadUtils().download(out ,response,"employee-template.xlsx");
-
-        }    }
     /**
      * 当月人事报表导出
      *  参数：
      *      年月-月（2018-02%）
      */
-    /*@RequestMapping(value = "/export/{month}", method = RequestMethod.GET)
-    public void export(@PathVariable String month) throws Exception {
-        //1.获取报表数据
-        List<EmployeeReportResult> list = userCompanyPersonalService.findByReport(companyId,month);
-        //2.构造Excel
-        //创建工作簿
-        //SXSSFWorkbook : 百万数据报表
-        //Workbook wb = new XSSFWorkbook();
-        SXSSFWorkbook wb = new SXSSFWorkbook(100); //阈值，内存中的对象数量最大数量
-        //构造sheet
-        Sheet sheet = wb.createSheet();
-        //创建行
-        //标题
-        String [] titles = "编号,姓名,手机,最高学历,国家地区,护照号,籍贯,生日,属相,入职时间,离职类型,离职原因,离职时间".split(",");
-        //处理标题
-
-        Row row = sheet.createRow(0);
-
-        int titleIndex=0;
-        for (String title : titles) {
-            Cell cell = row.createCell(titleIndex++);
-            cell.setCellValue(title);
-        }
-
-        int rowIndex = 1;
-        Cell cell=null;
-        for (EmployeeReportResult employeeReportResult : list) {
-            row = sheet.createRow(rowIndex++);
-            // 编号,
-            cell = row.createCell(0);
-            cell.setCellValue(employeeReportResult.getUserId());
-            // 姓名,
-            cell = row.createCell(1);
-            cell.setCellValue(employeeReportResult.getUsername());
-            // 手机,
-            cell = row.createCell(2);
-            cell.setCellValue(employeeReportResult.getMobile());
-            // 最高学历,
-            cell = row.createCell(3);
-            cell.setCellValue(employeeReportResult.getTheHighestDegreeOfEducation());
-            // 国家地区,
-            cell = row.createCell(4);
-            cell.setCellValue(employeeReportResult.getNationalArea());
-            // 护照号,
-            cell = row.createCell(5);
-            cell.setCellValue(employeeReportResult.getPassportNo());
-            // 籍贯,
-            cell = row.createCell(6);
-            cell.setCellValue(employeeReportResult.getNativePlace());
-            // 生日,
-            cell = row.createCell(7);
-            cell.setCellValue(employeeReportResult.getBirthday());
-            // 属相,
-            cell = row.createCell(8);
-            cell.setCellValue(employeeReportResult.getZodiac());
-            // 入职时间,
-            cell = row.createCell(9);
-            cell.setCellValue(employeeReportResult.getTimeOfEntry());
-            // 离职类型,
-            cell = row.createCell(10);
-            cell.setCellValue(employeeReportResult.getTypeOfTurnover());
-            // 离职原因,
-            cell = row.createCell(11);
-            cell.setCellValue(employeeReportResult.getReasonsForLeaving());
-            // 离职时间
-            cell = row.createCell(12);
-            cell.setCellValue(employeeReportResult.getResignationTime());
-        }
-
-        //3.完成下载
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        wb.write(os);
-        new DownloadUtils().download(os,response,month+"人事报表.xlsx");
-    }*/
-
-
-
-
-    /**
-     *  采用模版打印的形式完成报表导出,sxssf不支持模版导出
-     */
     @RequestMapping(value = "/export/{month}", method = RequestMethod.GET)
-    public void export(@PathVariable String month) throws Exception {
-        //1.构造报表数据
-        List<EmployeeReportResult> list = userCompanyPersonalService.findByReport(companyId , month);
-        //2.加载模版
+    public void export(@PathVariable(name = "month") String month) throws Exception {
+        //1.构造数据
+        List<EmployeeReportResult> list =
+                userCompanyPersonalService.findByReport(companyId,month+"%");
+        //2.加载模板流数据
         Resource resource = new ClassPathResource("excel-template/hr-demo.xlsx");
         FileInputStream fis = new FileInputStream(resource.getFile());
+        new ExcelExportUtil(EmployeeReportResult.class,2,2).
+                export(response,fis,list,"人事报表.xlsx");
 
-
-        //3.通过工具类下载文件
-        new ExcelExportUtil(EmployeeReportResult.class , 2, 2)
-                .export(response , fis , list , month + "人事报表.xlsx");
-
+        fis.close();
     }
 
 }
