5e7f9fa83dff491dd5199dd8e76786abdd60623b
test <test>
2022-01-08 23:06:16 +0800

    fix 之前忘记添加gateway转发规则了

--- ihrm_gate/src/main/java/com/ihrm/gate/GateApplication.java
+++ ihrm_gate/src/main/java/com/ihrm/gate/GateApplication.java
@@ -5,11 +5,12 @@ import com.ihrm.common.utils.IdWorker;
 import com.ihrm.common.utils.JwtUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = "com.ihrm")
+@SpringBootApplication(scanBasePackages = "com.ihrm", exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableZuulProxy
 public class GateApplication {
