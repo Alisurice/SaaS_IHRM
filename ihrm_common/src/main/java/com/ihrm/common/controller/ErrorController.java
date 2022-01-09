e71d2ee857cde274d423982f4ecdece049caaa6d
test <test>
2022-01-09 12:47:47 +0800

    fixed eureka路径配置错误（多了一个/），导致gateway超时

--- ihrm_common/src/main/java/com/ihrm/common/controller/ErrorController.java
+++ ihrm_common/src/main/java/com/ihrm/common/controller/ErrorController.java
@@ -2,14 +2,13 @@ package com.ihrm.common.controller;
 
 import com.ihrm.common.entity.Result;
 import com.ihrm.common.entity.ResultCode;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author: hyl
- * @date: 2020/02/08
- **/
+
 @RestController
+@CrossOrigin
 public class ErrorController {
 
     //公共错误跳转
