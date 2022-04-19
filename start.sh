80f817b9d4b959b9b46207fb72131e11ebb150ed
alicloud <alisurice@com>
2022-04-19 22:36:33 +0800

    feat 增加本地执行脚本，配置jvm，降低内存消耗。 增加sql运行脚本可以自动执行sql函数，为mysql设定远程访问账号，密码参考Dockerfile？或者可能在docker-compose里面

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+path=$(pwd)
+logdir=$path/log/
+echo "log dir: "$logdir
+mkdir $logdir 2>/dev/null
+
+service_list=(ihrm_eureka ihrm_gate ihrm_system ihrm_company ihrm_employee ihrm_social_securitys ihrm_attendance ihrm_audit)
+
+for service in ${service_list[@]}
+do
+  echo "start "$service" ?"
+  read start_service
+  cd $path/$service/target/
+  pwd
+  run_java="-Xmx64m -Xms16m -jar ${service}-1.0-SNAPSHOT.jar"
+  nohup java $run_java > $logdir$service".log" 2>&1 &
+done
+
